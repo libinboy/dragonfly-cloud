280bc1e45531aada307a1a2a9af6c49431f59a71
libinboy <903603502@qq.com>
2017-10-20 16:49:58 +0800

    add
    Signed-off-by: libinboy <903603502@qq.com>

---
+++ dragonfly-ui/src/main/java/com/dragonfly/cloud/ui/UiApplication.java
@@ -0,0 +1,18 @@
+package com.dragonfly.cloud.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by Administrator on 2017/7/11 0011.
+ */
+// @EnableEurekaClient
+@SpringBootApplication
+public class UiApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(UiApplication.class, args);
+    }
+}
